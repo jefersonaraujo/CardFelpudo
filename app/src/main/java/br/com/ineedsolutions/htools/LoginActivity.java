267ce421b23fd95cfe9039586593a0e0d5b528b0
Jeferson Araujo <jefersonaraujo95@gmail.com>
2018-03-29 18:43:38 -0300

    Update

---
+++ app/src/main/java/br/com/ineedsolutions/htools/LoginActivity.java
@@ -0,0 +1,79 @@
+package br.com.ineedsolutions.htools;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth nAuth;
+    private EditText editEMail;
+    private EditText editSenha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+
+        nAuth = FirebaseAuth.getInstance();
+        editEMail = findViewById(R.id.campoEmail);
+        editSenha = findViewById(R.id.campoSenha);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = nAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    private  void updateUI(FirebaseUser user){
+        if(user != null){
+            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+    public void login(View view){
+        String email = editEMail.getText().toString().trim();
+        String senha = editSenha.getText().toString().trim();
+
+        if(email.equals("")){
+            editEMail.setError("Preencha este Campo !");
+            return;
+        }
+        if(senha.equals("")){
+            editSenha.setError("Preencha este Campo !");
+            return;
+        }
+        nAuth.signInWithEmailAndPassword(email,senha).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    updateUI(nAuth.getCurrentUser());
+                }else{
+                    try{
+                        throw  task.getException();
+                    }catch (Exception e){
+                        Log.e("Login", e.getMessage());
+                    }
+                    Toast.makeText(LoginActivity.this, "Usuário ou Senha Incorreto !", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/br/com/ineedsolutions/htools/MainActivity.java
@@ -0,0 +1,121 @@
+package br.com.ineedsolutions.htools;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private  String uid;
+    private  String Usuario;
+    private ArrayList<String> seguindo;
+
+
+    //private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    Intent i = new  Intent(MainActivity.this, CadastroUserActivity.class);
+//                    mTextMessage.setText(R.string.title_home);
+                      return true;
+//                case R.id.navigation_dashboard:
+//                    mTextMessage.setText(R.string.title_dashboard);
+//                    return true;
+//                case R.id.navigation_notifications:
+//                    mTextMessage.setText(R.string.title_notifications);
+//                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        mTextMessage = (TextView) findViewById(R.id.message);
+//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FirebaseDatabase database =  FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("nomes");
+        myRef.setValue("Hello World");
+
+        /*Comecar*/
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        seguindo = new ArrayList<>();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user == null) finish();
+
+    }
+    private void getUserInfo(){
+        uid = mAuth.getCurrentUser().getUid();
+        DatabaseReference userRef = database.getReference("users/"+ uid);
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Usuario = dataSnapshot.child("usuario").getValue(String.class);
+                seguindo.clear();
+                for(DataSnapshot s :dataSnapshot.child("seguindo").getChildren()){
+                    seguindo.add(s.getValue(String.class));
+
+                }
+                Log.d("usuario",Usuario);
+                Log.d("lista",seguindo.toString());
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.sair){
+            mAuth.signOut();
+            finish();
+        }
+        return  true;
+    }
+}
